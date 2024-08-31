d6876e5518998d8d02a97fd6f3cfe7a788369f60
NK <nils@inlage.com>
2024-08-31 13:47:31 +0200

    .NET 8.0

--- Program.cs
+++ Program.cs
@@ -1,23 +1,17 @@
-﻿using Mahjongg;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WindowsFormsApp1
+namespace MahjonggCalculator
 {
     internal static class Program
     {
         /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Players());
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Mahjongg.Players());
         }
     }
 }
